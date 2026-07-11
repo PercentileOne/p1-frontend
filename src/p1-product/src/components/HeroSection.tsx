20c375b4cd0f8b941c93f717088980b57302592f
PercentileOne <francis@percentile.one>
2026-07-11 13:00:41 +0100

    Replace scale+negative-margin hack with zoom:0.72 on mobile mockup

--- src/p1-product/src/components/HeroSection.tsx
+++ src/p1-product/src/components/HeroSection.tsx
@@ -62,10 +62,8 @@ export default function HeroSection() {
           </div>
 
           {isMobile ? (
-            <div style={{ width: '100%', overflowX: 'hidden', marginBottom: -80 }}>
-              <div style={{ transform: 'scale(0.72)', transformOrigin: 'top left', width: '139%' }}>
-                <AppMockup />
-              </div>
+            <div style={{ zoom: 0.72, width: '100%', overflowX: 'hidden' }}>
+              <AppMockup />
             </div>
           ) : (
             <AppMockup />
